300fc2bf7bb95a326d3e738fda71aba185802a7d
Dan01011997 <f2016915@pilani.bits-pilani.ac.in>
2025-08-15 03:01:04 +0530

    prettier config done

--- webpack.common.js
+++ webpack.common.js
@@ -1,37 +1,36 @@
 // webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ESLintPlugin = require('eslint-webpack-plugin');
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-     publicPath: '/',
-         assetModuleFilename: 'images/[hash][ext][query]',
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/",
+        assetModuleFilename: "images/[hash][ext][query]",
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
         new ESLintPlugin({
-      extensions: ['js'],
-      emitWarning: true,
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+            extensions: ["js"],
+            emitWarning: true,
+        }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,15 @@
-   const path = require("path");
-   const { merge } = require('webpack-merge');
- const common = require('./webpack.common');
- 
-   module.exports = merge(common, {
-   mode: 'development',
-   devtool: 'eval-source-map',
-   devServer: {
-      static: {
-      directory: path.resolve(__dirname, "dist"), // needed for asset/resource to serve from dist
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "eval-source-map",
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist"), // needed for asset/resource to serve from dist
+        },
+        watchFiles: ["./src/template.html"],
+        open: true,
     },
-    watchFiles: ["./src/template.html"],
-    open:true,
-   },
- });
+});
